c4bd83465304dadb9662282d2d292e61cc6bb9bd
mikhail-bezumov <mikhail.bezumov@gmail.com>
2025-04-23 00:25:41 +0200

    Initial commit

---
+++ Задание 2_1Про/app.js
@@ -0,0 +1,5 @@
+const user = 'Mikhail';
+let age = 35;
+let city = 'Arkhangelsk';
+let hobby = 'Webflow, football';
+console.log('Меня зовут', user, 'мой возраст', age,'город проживания', city,'хобби', hobby)
