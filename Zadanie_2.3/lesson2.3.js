ade0583eb064dd61639923dc306e510fadd910dd
mikhail-bezumov <mikhail.bezumov@gmail.com>
2025-04-28 00:43:06 +0200

    lesson2.3

---
+++ Zadanie_2.3/lesson2.3.js
@@ -0,0 +1,19 @@
+//const age = 27;
+//console.log(typeof age);
+//const bigItn = 123141234203n;
+//console.log(typeof bigItn);
+//const firstName = 'Name'
+//console.log(typeof firstName);
+
+const titleProject = 'оценка стоимости'
+console.log(typeof titleProject);
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+console.log(typeof screensValue);
+const screenPrice = 5000;
+console.log(typeof screenPrice);
+const percentage  = 500;
+console.log(typeof percentage);
+const fullPrice  = 50000;
+console.log(typeof fullPrice);
+let responsive = true;
+console.log(typeof responsive);
